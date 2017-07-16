535dde07f97f1113986db2313f78c619c417595a
mdd <mdd@mdddeMacBook-Pro.local>
2017-07-16 21:09:16 +0800

    Initial Commit

---
+++ LinaHitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LinaHitTest
+//
+//  Created by mdd on 2017/7/16.
+//  Copyright © 2017年 mdd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
